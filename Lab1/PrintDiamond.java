2dee0832110f162308682506661796ea79304547
Jennifer J. Cruz-Hernandez <jennifercruz0813@gmail.com>
2018-11-07 17:58:22 -0800

    Create PrintDiamond.java

---
+++ Lab1/PrintDiamond.java
@@ -0,0 +1,16 @@
+/** A program that prints out a diamond shape.
+	@author Jennifer J.Cruz Hernandez
+**/
+
+public class PrintDiamond {
+
+	public static void main(String[] args) {
+		System.out.println("   * ");
+		System.out.println("  * *");
+		System.out.println(" * * *");
+		System.out.println("* * * *");
+		System.out.println(" * * *");
+		System.out.println("  * *");
+		System.out.println("   *");
+	}
+}
