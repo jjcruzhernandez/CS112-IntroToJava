a15b6e172c1dd38a86fc9b4839fc200d8c0c0fa4
Jennifer J. Cruz-Hernandez <jennifercruz0813@gmail.com>
2018-11-07 17:57:58 -0800

    Create Initials.java

---
+++ Lab1/Initials.java
@@ -0,0 +1,16 @@
+/** A program that prints out my initials.
+	@author Jennifer J.Cruz Hernandez
+**/
+
+public class Initials {
+
+	public static void main(String[] args) {
+		System.out.println("JJJJJJJJJ JJJJJJJJJ  CCCCCCC  H     H");
+		System.out.println("    J         J      C      C H     H");
+		System.out.println("    J         J      C        H     H");
+		System.out.println("    J         J      C        HHHHHHH");
+		System.out.println(" J  J      J  J      C        H     H");
+		System.out.println(" J  J      J  J      C      C H     H");
+		System.out.println("  JJJ       JJJ      CCCCCCC  H     H");
+	}
+}
