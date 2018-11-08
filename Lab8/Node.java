ce81ea84365a9d1188eb73ea12bb6e41b45fadc6
Jennifer J. Cruz-Hernandez <jennifercruz0813@gmail.com>
2018-11-07 18:16:00 -0800

    Create Node.java

---
+++ Lab8/Node.java
@@ -0,0 +1,31 @@
+/**
+	A class to represent a LinkedList node containing
+	an String.
+**/
+public class Node {
+
+	private Integer data;
+	private Node next;
+
+	public Node(Integer data) {
+		this.data = data;
+		this.next = null;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public void setData(Integer data) {
+		this.data = data;
+	}
+
+	public Integer getData() {
+		return this.data;
+	}
+
+	public Node getNext() {
+		return this.next;
+	}
+
+}
